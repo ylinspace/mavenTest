e92e3b3f6d642d6e1068e95b8ae6666ba9e80b90
Yanglin <y_linspace@163.com>
2019-04-11 19:56:41 +0800

    ttt

--- src/main/java/org/cute/Test.java
+++ src/main/java/org/cute/Test.java
@@ -1,11 +1,28 @@
 package org.cute;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Test {
 
     public static void main(String [] args) throws Exception {
 
+        Date dd = new Date( 1554511684000L);
+
+        System.out.println(dd);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(dd);
+
+        c.add(Calendar.HOUR_OF_DAY,-12);
+
+        Date start = c.getTime();
+
+        c.add(Calendar.HOUR_OF_DAY,24);
+        Date end = c.getTime();
 
 
+        System.out.println("start"+start+";end"+end);
     }
 
 }
